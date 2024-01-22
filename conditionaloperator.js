4c1817783125728b0d4e3833d6e7689bfdfbb3c8
INDHUPRIYA-D-80 <indhupriyad.21ece@kongu.edu>
2024-01-22 21:47:25 +0530

    Fourth commit

--- conditionaloperator.js
+++ conditionaloperator.js
@@ -39,3 +39,33 @@ else
 }
 
 
+//switch case
+let taken = "Papaya";
+
+switch(taken) {
+    case "Apple":
+        console.log("You have chosen Apple\nLooks like a good choice.\nIt's good for health and is rich in antioxidants and fiber.");
+        break;
+    case "Banana":
+        console.log("You have chosen Banana\nGreat choice!\nIt has high potassium content, aids digestion, and provides energy.");
+        break;
+    case "Mango":
+        console.log("You have chosen Mango\nI thought you had a delicious pick.\nIt is packed with vitamins, boosts immunity, and supports eye health.");
+        break;
+    case "Papaya":
+        console.log("You have chosen Papaya\nEnjoy the tropical taste on your tongue.\nLoaded with enzymes, it promotes digestion and skin health.");
+        break;               
+    default:
+        console.log("Ohhhhh...! It's a bitter thing... Sorry! Invalid Option");
+        break;
+}
+
+
+//Ternary Operator
+
+let marks = 80;
+result = (marks>=35)?"Passed":"Failed";
+console.log("Rani!...You have",result,"in the exam")
+let marks1 = 30;
+result1 = (marks1>=40)?"Passed":"Failed";
+console.log("Meena!...You have",result1,"in the exam")
